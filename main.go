56ad6b899b7e1c6d1d5b75f40df061e7ac3fa985
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-01-27 17:58:52 +0200

    goflatten: Adding duplicate file detetion

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ var rootDir = flag.String("rootDir", ".", "The root directory to start from")
 var skipHidden = flag.Bool("skipHidden", true, "True if hidden files and directories should be skipped.")
 var dryRun = flag.Bool("dryRun", true, "A dryrun prints out all the file moves instead of actually moving them.")
 
-func flattenDirectories(path string, info os.FileInfo, err error) error {
+var dupes = make(map[string]bool)
+
+func flattenDirectories(path string, info os.FileInfo, _ error) error {
 	if info.IsDir() {
 		n := info.Name()
 		if len(n) > 1 && strings.HasPrefix(n, ".") {
@@ -21,6 +23,13 @@ func flattenDirectories(path string, info os.FileInfo, err error) error {
 		return nil
 	}
 	np := fmt.Sprintf("%s%c%s", *rootDir, os.PathSeparator, info.Name())
+
+	if dupes[np] {
+		fmt.Printf("WARNING: Duplicate file %s found.\n", np)
+	} else {
+		dupes[np] = true
+	}
+
 	return moveFile(path, np, *dryRun)
 }
 
